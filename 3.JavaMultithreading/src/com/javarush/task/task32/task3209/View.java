fbd074259de52267a52962ee3414e9b720817a99
Alexei Kurbatov <vinsler@yandex.ru>
2018-04-28 22:52:30 +0300

    test big task

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -1,8 +1,12 @@
 package com.javarush.task.task32.task3209;
 
+import com.javarush.task.task32.task3209.listeners.FrameListener;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowStateListener;
 
 public class View extends JFrame implements ActionListener {
     private JTabbedPane tabbedPane = new JTabbedPane();
@@ -29,6 +33,25 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void init() {
+        initGui();
+        FrameListener listener = new FrameListener(this);
+        addWindowListener(listener);
+        setVisible(true);
+    }
+
+    public void initMenuBar(){
 
     }
+
+    public void initEditor(){
+
+    }
+
+    public void initGui(){
+        initMenuBar();
+        initEditor();
+        pack();
+    }
+
+
 }
