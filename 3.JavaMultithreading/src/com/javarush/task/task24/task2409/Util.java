d4feec3927eba21f8a13435e32f062da760085b8
Alexei Kurbatov <vinsler@yandex.ru>
2018-05-03 20:48:21 +0300

    3.jr multh 24 lvl 5+o

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2409/Util.java
@@ -0,0 +1,112 @@
+package com.javarush.task.task24.task2409;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import static com.javarush.task.task24.task2409.Util.Company.Denim;
+import static com.javarush.task.task24.task2409.Util.Company.Levis;
+
+public class Util {
+    protected static Collection<Object[]> jeansArray = new LinkedList<>();
+    // переменная типа Коллекции с Object внутри и реализацией листа, но блядь list.get() там с ука нет.
+
+    static {
+        jeansArray.add(new Object[]{1, Levis, 34, 6, 150.0});
+        jeansArray.add(new Object[]{2, Denim, 35, 8, 154.0});
+        jeansArray.add(new Object[]{3, Company.Colins, 32, 6, 120.0});
+        jeansArray.add(new Object[]{4, Company.CalvinKleinJeans, 31, 8, 125.0});
+    }
+
+    public static List<Jeans> getAllJeans() {
+
+        List<Jeans> allJeans = new LinkedList<>();
+
+        for (Object[] obj : getJeansArray()) {
+            int id = (int) obj[0];
+            final Company company = (Company ) obj[1];
+            int length = (int) obj[2];
+            int size = (int) obj[3];
+            double price = (double) obj[4];
+
+            abstract class AbstractJeans implements Jeans {
+                public AbstractJeans(int id, int length, int size, double price) {
+                }
+
+                @Override
+                public int getLength() {
+                    return length;
+                }
+                @Override
+                public int getSize() {
+                    return size;
+                }
+                @Override
+                public int getId() {
+                    return id;
+                }
+                @Override
+                public double getPrice() {
+                    return price;
+                }
+                @Override
+                public String getTM() {
+                    return company.fullName;
+                }
+                @Override
+                public String toString() {
+                    return super.toString();
+                }
+            }
+
+            class Levis extends AbstractJeans {
+                public Levis(int id, int length, int size, double price) {
+                    super(id, length, size, price);
+                }
+            }
+
+            class Denim extends AbstractJeans {
+                public Denim(int id, int length, int size, double price) {
+                    super(id, length, size, price);
+                }
+            }
+
+            Jeans jeans = null;
+
+            if (Levis == company) {
+                jeans = new Levis(id, length, size, price);
+            } else
+                if (Denim == company) {
+                    jeans = new Denim(id, length, size, price);
+                } else {
+                    jeans = new AbstractJeans(id, length, size, price) {
+                        @Override
+                        public String toString() {
+                            return super.toString();
+                        }
+                        public String getTM() {
+                            return company.fullName;
+                        }
+                    };
+                }
+            allJeans.add(jeans);
+        }
+        return allJeans;
+    }
+
+    public static Collection<Object[]> getJeansArray() {
+        return jeansArray;
+    }
+
+    static enum Company {
+        Levis ("Levi's"),
+        Denim("Denim"),
+        Colins("COLIN'S"),
+        CalvinKleinJeans("Calvin Klein Jeans");
+
+        final String fullName;
+        Company(String name) {
+            this.fullName = name;
+        }
+    }
+}
