731d5c0b848a2fc3e9b40482067e96953fac221f
Alexei Kurbatov <vinsler@yandex.ru>
2018-07-05 20:07:29 +0300

    4.jr collect 11-12-13 tasks

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3211/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task32.task3211;
+
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
+/* 
+Целостность информации
+*/
+
+public class Solution {
+    public static void main(String... args) throws Exception {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(new String("test string"));
+        oos.flush();
+        System.out.println(compareMD5(bos, "5a47d12a2e3f9fecf2d9ba1fd98152eb")); //true
+
+    }
+
+    public static boolean compareMD5(ByteArrayOutputStream byteArrayOutputStream, String md5) throws Exception {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.reset();
+        md.update(byteArrayOutputStream.toByteArray());
+        String s2 = new BigInteger(1, md.digest()).toString(16);
+        while (s2.length() < 32) {
+            s2 = "0" + s2;
+        }
+        return s2.equals(md5);
+    }
+}
